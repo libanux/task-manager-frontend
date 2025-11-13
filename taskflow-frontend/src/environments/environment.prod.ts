e55f22246a1cae4e609efad6e1a735e92fb14c3e
unknown <marwamarwazehri@gmail.com>
2025-11-13 16:57:56 +0200

    deploy to github pages

--- taskflow-frontend/src/environments/environment.prod.ts
+++ taskflow-frontend/src/environments/environment.prod.ts
@@ -2,15 +2,3 @@ export const environment = {
   production: true,
   apiUrl: 'https://task-manager-backend-oez7.onrender.com/api'
 };
-
-
-
- /*this is Production Environment
-
-Backend: Runs on Render.com
-
-Database: Live production database (real user data)
-
-Frontend: Connects to deployed backend
-
-Result: Real users interact with this */
--- taskflow-frontend/src/environments/environment.ts
+++ taskflow-frontend/src/environments/environment.ts
@@ -1,16 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'http://localhost:3000'
 };
 
-
-
-/* this is  Local Development Environment:
-
-Backend: Runs on your machine (localhost:3000)
-
-Database: Your local database (empty or test data)
-
-Frontend: Connects to local backend
-
-Result: Your testing doesn't affect real users */
